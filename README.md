21c1ab3baa971ee79586ece91068d0b5b253ee00
asanders4205 <asanders4205@gmail.com>
2025-09-03 19:52:02 -0400

    Image resize logic - naming and saving resized images

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 # Image Quality Classifier #
 
 ## The goal of this process is to classify images as good quality or bad quality! ##
+
 We will be using the BIQ2021 dataset found on Kaggle here:\
 https://www.kaggle.com/datasets/nisarahmedrana/biq2021
+
+
+Uses python 3.9+ \
+Programmed on Windows, may not be portable. Modifications to follow 
 
 The processing will be done step-by-step\
     and will reference the Knowledge Discovery in Databases (KDD) process outlined below
@@ -24,6 +29,8 @@ Knowledge/Deployment:  use insights in decision-making
 
 
 ## To get started ##
+Activate a virtual environment if you so choose \
+Ensure you are using python 3.9+ \
 Create a 'data' folder in your project directory \
 In your terminal run 'pip install -r requirements.txt' to install requirements \
 Execute the code to download the dataset via this line\
--- main.py
+++ main.py
@@ -3,24 +3,19 @@ import kagglehub
 from PIL import Image, UnidentifiedImageError
 import shutil
 
-path = kagglehub.dataset_download("nisarahmedrana/biq2021")
 
 
-def verify_files(path: str) -> None:
+
+def verify_files(path: str, target_size: tuple[int,int] = (512, 512)) -> None:
+    ''' verify_files
+        Verifies that all files in a provided directory are uniformly sized images
+        If a non-image file is found, move it to bad_files folder (created within the function)
+        Parameters:
+            path: Filepath to folder containing images
+            param_size: Image size: Default is set to (512, 512)
+        Return: None
     '''
-        Accepts a filepath
-        Verifies that all files in the directory are images
-            - If a non-image file is found, move to bad_files folder (created within the function)
-            - Sets a uniform image size and resizes all images uniformly if necessary
-                - Sets to (512, 512) by default
-    '''
 
-    # Open an image
-    sample_image_path = os.path.join(path, "Images (1).jpg")  # adjust based on folder layout
-    img = Image.open(sample_image_path)
-
-
-    target_size = (512, 512)
     valid_exts = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"}
 
     # Repository for bad files
@@ -30,9 +25,10 @@ def verify_files(path: str) -> None:
 
     # Check each file in the directory
     for file_name in os.listdir(path):
+        resized_counter = 0 # Count and name resized images
+        prefix = 'resized_image_'
 
-        ''' Grab the file extension    # _ is a throwaway variable  ext will hold the file extension
-        '''
+        # Grab the file extension:   _ is a throwaway variable ext will hold the file extension
         _, ext = os.path.splitext(file_name)
 
         # Ensure lowercase
@@ -41,10 +37,10 @@ def verify_files(path: str) -> None:
         # Store location of image
         image_path = os.path.join(path,file_name)
 
+
         # Verify all files are images (have file extensions in the list of known image formats)
         if ext not in valid_exts:
             print(f'Non-image file found: {file_name}')
-            # image_path = os.path.join(path,file_name)
 
             # Move non-image files to other folder
             try:
@@ -55,12 +51,45 @@ def verify_files(path: str) -> None:
                 print(f'Moving {file_name} to bad_files/')
                 shutil.move(image_path, bad_dir)
 
-        # Open and find the image size
-        with Image.open(image_path) as img:
 
-            # Image is not the proper size (512, 512)
+
+
+        # Verify all images are of the same size
+        with Image.open(image_path) as img:     # Open image file and find the size
+
+            # Image is not the proper size
             if img.size != target_size:
+
+                # Give error message
                 # print(f"Improperly sized file: {file_name}: {img.size}")
+
+                # Resize the image and save
                 print(f'Resizing image {file_name}')
                 image_path = img.resize((512,512))
-                # img = img.resize((512,512)) # This does not resize the image, only the loop variable
+
+                updated_filename = f"{resized_counter}.{ext}" # A file counter for resized images, and the saved file extension
+
+                image_path.save(os.path.join(updated_filename))
+
+    print(f'Files validated. All images are of size {target_size}')
+
+
+def main():
+
+    data_path = kagglehub.dataset_download("nisarahmedrana/biq2021")
+
+    # Open an image
+    sample_image_path = os.path.join(data_path, "Images (1).jpg")  # TODO generalize and adjust based on folder layout
+    img = Image.open(sample_image_path)
+
+    #image_size = (512, 512)
+
+    verify_files(data_path)
+
+    #TODO rename all images so there is not a space in the name
+    #TODO resize_image function
+
+
+
+if __name__=="__main__": 
+    main()
