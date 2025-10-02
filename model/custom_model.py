c2361d14fdaef9df4258d925dcdb754623703577
asanders4205 <asanders4205@gmail.com>
2025-10-02 06:09:40 -0400

    training data rename setup

--- model/custom_model.py
+++ model/custom_model.py
@@ -1,23 +1,41 @@
-import numpy as np
+
+
+
+
+# '''Format the BIQ2021 image label file'''
+# def process_filenames_in_labelled_data(path: str):
+#     '''Remove certain characters from filenames
+#     '''
+#     print('Processing filenames')
+#     replacements = {" ":"_", "(":"", ")":""}
+#     files = sorted(os.listdir(path))
+#     counter = 0
+
+#     for file_name in files:
+#         new_name = file_name
+#         # counter += 1
+#         for old,new in replacements.items():
+#             new_name = new_name.replace(old, new)
+
+#         if new_name != file_name: # Rename if changed
+#             old_path = os.path.join(path, file_name)
+#             new_path = os.path.join(path, new_name)
+#             os.rename(old_path, new_path)
+#             counter += 1
+
+#     print(f'Filenames processed: {counter} filenames changed')
+
+
+
+
+'''Convert to pandas df
+iterate over rows
+in col 0 replace certain chars with others'''
+
 import pandas as pd
-import matplotlib as plt
+data = pd.read_csv('data\BIQ2021.csv')
 
-import torch
-from torch import optim
-from torch import nn
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 
-import torchvision
-
-''' Train model on MNIST dataset'''
-
-batch_size = 60
-
-train_dataset = datasets.MNIST(root="dataset/", download=True, train=True, transform=transforms.ToTensor())
-
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-
-test_dataset = datasets.MNIST(root="dataset/", download=True, train=False, transform=transforms.ToTensor())
-
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+for i, row in data.head(2).iterrows():
+    new_col_name = data.iloc[i,0]
+    print(new_col_name)
